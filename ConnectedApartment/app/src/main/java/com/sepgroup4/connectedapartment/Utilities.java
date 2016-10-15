08f03e8739cc32edbbc007c17c9059484c9deaad
Kim An Lim <11883170@student.uts.edu.au>
2016-10-15 19:27:20 +1100

    added getFormattedDate method

--- ConnectedApartment/app/src/main/java/com/sepgroup4/connectedapartment/Utilities.java
+++ ConnectedApartment/app/src/main/java/com/sepgroup4/connectedapartment/Utilities.java
@@ -38,4 +38,14 @@ public class Utilities {
         }
         return calendar;
     }
+
+    public static String getFormattedDate(Calendar calendar) {
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        String yearStr = String.format(Locale.UK, "%04d", year);
+        String monthStr = String.format(Locale.UK, "%02d", month);
+        String dayStr = String.format(Locale.UK, "%02d", day);
+        return yearStr + "/" + monthStr + "/" + dayStr;
+    }
 }
